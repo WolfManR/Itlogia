a2d01135b8c0b08004763313c783dee2fa78df1f
Tarxos <ftarxos@gmail.com>
2023-01-15 00:39:28 +0300

    add forms validators for price form, add code for navigation on page, add styles for buttons

--- cars-app/src/app/app.component.ts
+++ cars-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cars-app';
+  priceForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    car: ['', Validators.required],
+  })
+
+  constructor(private fb: FormBuilder) {
+  }
+    goScroll(target: HTMLElement){
+      target.scrollIntoView({behavior: "smooth"})
+    }
+
+    onSubmit(){
+      if(this.priceForm.valid){
+        alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!")
+        this.priceForm.reset()
+      }
+    }
 }
