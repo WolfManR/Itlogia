fe08752d06c0b9549784b8913090d65e76214ade
Tarxos <ftarxos@gmail.com>
2023-01-15 00:50:19 +0300

    replace templated code for car-item with angular templates and loop

--- cars-app/src/app/app.component.ts
+++ cars-app/src/app/app.component.ts
@@ -13,6 +13,52 @@ export class AppComponent {
     car: ['', Validators.required],
   })
 
+  carsData = [
+    {
+      image: "1.png",
+      name:"Lamborghini Huracan Spyder",
+      gear: "полный",
+      engine: 5.2,
+      places: 2
+    },
+    {
+      image: "2.png",
+      name:"Chevrolet Corvette",
+      gear: "полный",
+      engine: 6.2,
+      places: 2
+    },
+    {
+      image: "3.png",
+      name:"Ferrari California",
+      gear: "полный",
+      engine: 3.9,
+      places: 4
+    },
+    {
+      image: "4.png",
+      name:"Lamborghini Urus",
+      gear: "полный",
+      engine: 4.0,
+      places: 5
+    },
+    {
+      image: "5.png",
+      name:"Audi R8",
+      gear: "полный",
+      engine: 5.2,
+      places: 2
+    },
+    {
+      image: "6.png",
+      name:"Аренда Chevrolet Camaro",
+      gear: "полный",
+      engine: 2.0,
+      places: 4
+    },
+
+  ]
+
   constructor(private fb: FormBuilder) {
   }
     goScroll(target: HTMLElement){
