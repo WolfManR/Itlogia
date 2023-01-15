41495790f7c68f12e8202b8f649457d82e982091
Tarxos <ftarxos@gmail.com>
2023-01-15 23:01:52 +0300

    handle homework for lesson6 extended

--- lesson6/src/app/app.component.ts
+++ lesson6/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {AppService} from "./app.service";
 
@@ -28,10 +28,24 @@ export class AppComponent {
     this.ngOnInit();
   }
 
-    goScroll(target: HTMLElement){
-      target.scrollIntoView({behavior: "smooth"})
+  goScroll(target: HTMLElement, car?: any) {
+    target.scrollIntoView({behavior: "smooth"});
+    if (car) {
+      this.priceForm.patchValue({car: car.name});
     }
+  }
 
+  trans: any;
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
+  }
+
+  bgPos: any;
+  @HostListener('document:scroll', ['$event'])
+  onScroll() {
+    this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
+  }
     onSubmit(){
       if (!this.priceForm.valid) return;
 
