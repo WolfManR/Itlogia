acf7e2be22de8cc20b44b043a9f21c3589b2e038
Tarxos <ftarxos@gmail.com>
2023-01-15 17:02:28 +0300

    copy cars-app template to lesson6

---
+++ lesson6/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  priceForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    car: ['', Validators.required],
+  })
+  carsData: any
+
+  constructor(private fb: FormBuilder, private appService: AppService) {
+  }
+
+  ngOnInit(){
+    this.appService.getData().subscribe(data => this.carsData = data)
+  }
+
+    goScroll(target: HTMLElement){
+      target.scrollIntoView({behavior: "smooth"})
+    }
+
+    onSubmit(){
+      if (!this.priceForm.valid) return;
+
+      this.appService.sendQuery(this.priceForm.value)
+        .subscribe({
+          next: (response: any) => {
+            alert(response.message)
+            this.priceForm.reset()
+          },
+          error: (response) => {
+            alert(response.error.message)
+          }
+        })
+    }
+}
